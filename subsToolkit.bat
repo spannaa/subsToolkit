ae12ccc34397b8e75a19a653b6c76dc506904221
spannaa <spannaa@users.noreply.github.com>
2017-08-26 13:14:49 +0100

    Update subsToolkit.bat

--- subsToolkit.bat
+++ subsToolkit.bat
@@ -10,6 +10,7 @@ subsToolkit 0 2>> log.txt
 :SkipMe
 mode con:cols=100 lines=72
 set currentApp=None
+set apktool=apktool_2.2.4.jar
 set heapy=512
 set projectFolder=None
 java -version 
@@ -39,9 +40,13 @@ echo.
 echo   Spannaa @ XDA
 echo.
 echo  --------------------------------------------------------------------------------------------------
-echo   Current Project:    %projectFolder% 
+echo   Current apktool version:   %apktool%
 echo.
-echo   Current App:        %currentApp% 
+echo   Current Max Memory Size:   %heapy%mb
+echo  --------------------------------------------------------------------------------------------------
+echo   Current Project:           %projectFolder% 
+echo.
+echo   Current App:               %currentApp%
 echo  --------------------------------------------------------------------------------------------------
 echo.
 REM Check if there are any existing project folders and redirect to CreateProjectFolder if not
@@ -68,11 +73,13 @@ echo    5.  Decompile a system apk
 echo.
 echo    6.  Batch decompile all apks in a project folder
 echo.
-echo    7.  Set Max Memory Size (Default: 512mb, Currently %heapy%mb)
+echo    7.  Select version of apktool to work with
 echo.
-echo    8.  Setup, notes ^& credits
+echo    8.  Set Max Memory Size (Default: 512mb)
 echo.
-echo    9.  Quit
+echo    9.  Setup, notes ^& credits
+echo.
+echo   10.  Quit
 echo.
 echo  --------------------------------------------------------------------------------------------------
 echo.
@@ -83,12 +90,14 @@ if %menunr%==3 goto ApkSelect
 if %menunr%==4 goto DecompileUserApk
 if %menunr%==5 goto DecompileSystemApk
 if %menunr%==6 goto DecompileAll
-if %menunr%==7 goto MaxMemorySize
-if %menunr%==8 goto Help
-if %menunr%==9 goto Quit
+if %menunr%==7 goto ApktoolSelect
+if %menunr%==8 goto MaxMemorySize
+if %menunr%==9 goto Help
+if %menunr%==10 goto Quit
+
 REM If an out of range number is entered, redirect to OutOfRangeError
 if %menunr%==0 goto OutOfRangeError
-if %menunr% GTR 9 goto OutOfRangeError
+if %menunr% GTR 10 goto OutOfRangeError
 
 :ProjectFolderSelect
 set projectFolder=None
@@ -185,7 +194,7 @@ goto Pause
 if exist "%~dp0%projectFolder%\working\%currentApp%" rmdir /S /Q "%~dp0%projectFolder%\working\%currentApp%"
 echo.
 echo   Decompiling %currentApp%...
-java -Xmx%heapy%m -jar apktool.jar d "..\%projectFolder%\files_in\%currentApp%" -b -o "..\%projectFolder%\working\%currentApp%" > nul
+java -Xmx%heapy%m -jar %apktool% d "..\%projectFolder%\files_in\%currentApp%" -b -o "..\%projectFolder%\working\%currentApp%" > nul
 if errorlevel 1 goto Level1Error
 )
 echo.
@@ -210,16 +219,17 @@ goto Pause
 echo.
 REM Delete any installed frameworks and install new one(s)
 rmdir /S /Q %userprofile%\AppData\Local\apktool > nul
+rmdir /S /Q %userprofile%\apktool > nul
 REM set /A count=0
 for %%F in (../%projectFolder%/frameworks/*.apk) do (
 echo   Installing framework: %%F...
-java -jar apktool.jar if ..\%projectFolder%\frameworks\%%F > nul
+java -jar %apktool% if ..\%projectFolder%\frameworks\%%F > nul
 if errorlevel 1 goto Level1Error
 )
 echo.
 if exist "%~dp0%projectFolder%\working\%currentApp%" rmdir /S /Q "%~dp0%projectFolder%\working\%currentApp%"
 echo   Decompiling %currentApp%...
-java -Xmx%heapy%m -jar apktool.jar d "..\%projectFolder%\files_in\%currentApp%" -b -o "..\%projectFolder%\working\%currentApp%" > nul
+java -Xmx%heapy%m -jar %apktool% d "..\%projectFolder%\files_in\%currentApp%" -b -o "..\%projectFolder%\working\%currentApp%" > nul
 if errorlevel 1 goto Level1Error
 )
 echo.
@@ -238,10 +248,11 @@ goto Pause
 echo.
 REM Delete any installed frameworks and install new one(s)
 rmdir /S /Q %userprofile%\AppData\Local\apktool > nul
+rmdir /S /Q %userprofile%\apktool > nul
 REM set /A count=0
 for %%F in (../%projectFolder%/frameworks/*.apk) do (
 echo   Installing framework: %%F...
-java -jar apktool.jar if ..\%projectFolder%\frameworks\%%F > nul
+java -jar %apktool% if ..\%projectFolder%\frameworks\%%F > nul
 if errorlevel 1 goto Level1Error
 )
 echo.
@@ -251,7 +262,7 @@ for %%F in (../%projectFolder%/files_in/*.apk) do (
 set /A count+=1
 if exist "%~dp0%projectFolder%\working\%%F" rmdir /S /Q "%~dp0%projectFolder%\working\%%F"
 echo   Decompiling %%F...
-java -Xmx%heapy%m -jar apktool.jar d "..\%projectFolder%\files_in\%%F" -b -o "..\%projectFolder%\working\%%F" > nul
+java -Xmx%heapy%m -jar %apktool% d "..\%projectFolder%\files_in\%%F" -b -o "..\%projectFolder%\working\%%F" > nul
 if errorlevel 1 (echo   There was an error decompiling %%D - please check your log.txt
 echo.
 echo - Press any key to continue...
@@ -263,6 +274,25 @@ echo.
 echo   All decompiled apks can be found in your 
 echo   %projectFolder%\working folder
 goto Pause
+
+:ApktoolSelect
+cls
+echo.
+echo   Select an apktool to work with...
+echo.
+set /A count=0
+for %%F in (tools/apktool_*.jar) do (
+set /A count+=1
+set a!count!=%%F
+if /I !count! LEQ 9 echo    !count!.  %%F 
+if /I !count! GTR 9 echo   !count!.  %%F 
+)
+echo.
+set /P INPUT=- Enter its number: %=%
+if /I %INPUT% GTR !count! goto ApktoolSelectError
+if /I %INPUT% LSS 1 goto ApktoolSelectError
+set apktool=!a%INPUT%!
+goto ReStart
 
 :MaxMemorySize
 set /P INPUT=- Enter max size for java heap space in megabytes (eg 512) : %=%
@@ -278,7 +308,7 @@ echo  --------------------------------------------------------------------------
 echo.
 echo  SETUP
 echo.
-echo  1. Java MUST be installed for this tool to work.
+echo  1. Java MUST be installed for this tool to work. Java 7 (JRE 1.7) is recommended.
 echo.
 echo  2. Create a project folder to work in - this could be named after the rom you're working
 echo     with, theme ready gapps or just a generic folder name if you're only working with user apps.
@@ -286,8 +316,8 @@ echo.
 echo  3. Copy ALL of the framework apks from the rom you're working with into the 'frameworks'
 echo     folder of the project folder.
 echo.
-echo  4. Copy all of the apks from the rom you're working with into the 'files_in' folder of the 
-echo     project folder.
+echo  4. Copy the apks you want to theme from the rom you're working with into the 'files_in' 
+echo     folder of the project folder.
 echo.
 echo  5. Use the menu to select tasks and execute them.
 echo.
@@ -300,21 +330,25 @@ echo.
 echo  Any number of self-contained project folders can be created and worked with and each 
 echo  project folder can contain any number of apks.
 echo.
-echo  Running Cleanup.bat after quitting will delete all installed frameworks and log.txt
+echo  New releases of apktool can be found at: https://ibotpeaches.github.io/Apktool/
+echo  To update subsToolkit, download the new apktool_x.x.x.jar, add it to the 'tools' folder 
+echo  and select it via option #7 in the subsToolkit menu.
 echo.
-echo  The toolkit currently uses apktool_2.2.4.jar. To switch to a different version, copy any 
-echo  apktool_2.x.x.jar version into the 'tools' folder and rename it 'apktool.jar'
-echo.
-echo  To clone, build and update apktool in subsToolkit, run update_subsToolkit.bat
+echo  To build and add the latest snapshot version of apktool to subsToolkit (git MUST be installed) 
+echo  run build_apktool_snapshot.bat, wait for the build to complete and then select the newly built
+echo  snapshot via option #7 in the subsToolkit menu.
 echo.
 echo  The default maximum memory (heap) size is '512'mb 
-echo  This should not need to be changed unless there is aproblem with decompiling/compiling.
+echo  This should not need to be changed unless there is aproblem with decompiling.
+echo.
+echo  Running Cleanup.bat after quitting will delete all installed frameworks and log.txt
 echo.
 echo  CREDITS
 echo.
 echo  subsToolkit is based on apkToolkit: Spannaa @ XDA
 echo  apkToolkit is based on Apk Manager: Daneshm90 @ XDA
-echo  apktool.jar: iBotPeaches @ XDA & Brut.all @ XDA
+echo  apktool: iBotPeaches @ XDA & Brut.all @ XDA
+echo  7za standalone command line version of 7-Zip: Igor Pavlov
 echo.
 echo  --------------------------------------------------------------------------------------------------
 goto Pause
@@ -338,6 +372,12 @@ goto Pause
 
 :ApkSelectError
 set currentApp=None
+echo.
+echo   You selected a number that wasn't one of the options^^!
+goto Pause
+
+:ApktoolSelectError
+set apktool=apktool_2.2.4.jar
 echo.
 echo   You selected a number that wasn't one of the options^^!
 goto Pause
